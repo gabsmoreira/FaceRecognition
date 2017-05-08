6710a4acfff8b502d4a71098de782cd580726aa9
Filipe Foschini Borba <filipefborba@gmail.com>
2017-05-08 13:59:35 -0300

    some comments

--- training.py
+++ training.py
@@ -7,6 +7,7 @@ import scipy.misc
 import matplotlib.pyplot as plt
 import sys
 
+#Scikit-learn imports (machine learning in Python)
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import fetch_lfw_people
@@ -15,29 +16,28 @@ from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
-name =["Leonardo","Borba","gabriel"]
+
+name = ["Leonardo","Borba","gabriel"] #all names the program will recognize
 photo_list = []
 mat = np.zeros(((len(name))*40,480*480))
 os.chdir("database")
-for i in range (len(name)):
-    os.chdir('db_{0}'.format(name[i]))
+
+#Reading all images in person's database to begin training
+for i in range(len(name)):
+    os.chdir('db_{0}'.format(name[i])) #db_name will be the folder with person's photos
     for j in range (1,41):
         image = cv2.imread('{0}_{1}.jpg'.format(name[i],j))
-        #print(image.shape)
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         image_flat_gray = image_gray.flat
         mat[i*40+j-1] = image_flat_gray
         if j==40:
             os.chdir("../")
 
-
 pca = PCA(n_components=100, svd_solver='randomized',whiten=True).fit(mat)
 
 eigenfaces = pca.components_.reshape((100, 480, 480))
 
-
 os.chdir("../")
-os.mkdir("eigenfaces")
 os.chdir("eigenfaces")
 for i in range(len(eigenfaces)):
     scipy.misc.imsave('eigenfaces_{0}.jpg'.format(i),eigenfaces[i])
